03f59fd1e2bc71ccdb777c51c908dbc8cc0b067e
gjlander <garrett.jlander@gmail.com>
2023-11-06 14:46:32 +0100

    all pokemon routes moved to separate folder and js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config(); u can write it here or in package.json (scripts),
 //  to add env file functionality. normally vite does it automatically in frontend
 const express = require("express");
+const pokemonRouter = require("./routes/pokemonRoutes.js");
 const app = express();
 // const dbPool = require("./db/pgClient");
 const {
@@ -30,9 +31,10 @@ app.get("/", (req, res) =>
     )
 );
 
-app.route("/pokemon").get(getAllPokemons);
-app.route("/pokemon/:pokeName").get(getSinglePokemon);
-app.route("/pokemon/withid/:id").get(getPokemonById);
-app.route("/pokemon/:pokeName/:info").get(getPokeInfo);
+app.use("/pokemon", pokemonRouter);
+// app.route("/pokemon").get(getAllPokemons);
+// app.route("/pokemon/:pokeName").get(getSinglePokemon);
+// app.route("/pokemon/withid/:id").get(getPokemonById);
+// app.route("/pokemon/:pokeName/:info").get(getPokeInfo);
 
 app.listen(port, () => console.log(`Welcome ${port}`));
