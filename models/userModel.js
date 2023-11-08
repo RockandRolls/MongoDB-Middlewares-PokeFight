9c6a55d31c62a102eac0ffad5d70a6bfe44bffdf
gjlander <garrett.jlander@gmail.com>
2023-11-08 14:22:23 +0100

    password selected for now durng dev

--- models/userModel.js
+++ models/userModel.js
@@ -43,7 +43,7 @@ const userSchema = new Schema(
         password: {
             type: String,
             required: true,
-            select: false,
+            //  select: false,
         },
 
         battlesWon: {
